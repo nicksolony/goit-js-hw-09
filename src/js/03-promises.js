baf91ae291312f34c439e513eb4e62f4ad794857
nicksolony <nicksolony@gmail.com>
2023-08-02 17:51:55 -0400

    created formObj on form submission

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,27 @@
+let delayInput = document.querySelector('input[name=delay]');
+let stepInput = document.querySelector('input[name=step]');
+let amountInput = document.querySelector('input[name=amount]');
+let formEl = document.querySelector('form'); 
+
+
+formEl.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+  const form = Array.from(e.target);
+  const formObj = {};
+  form.filter((el)=>el.type==='number').forEach ((input) => {
+    formObj[`${input.name}`]= input.value;
+  })
+  
+  console.log(formObj);
+
+})
+
+
+
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +29,6 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
-}
+};
+
+
