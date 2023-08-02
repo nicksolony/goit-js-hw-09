3817b8a054dd7343a7d32b57557ca7e339bd7c18
nicksolony <54452819+nicksolony@users.noreply.github.com>
2023-08-02 01:56:19 +0000

    Deploying to gh-pages from @ nicksolony/goit-js-hw-09@6c9386a4a18941a713bda18141521dd459225779 🚀

---
+++ 01-color-switcher.bd103e76.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){console.log("start")})),e.addEventListener("click",(function(){console.log("stop")}))}();
+//# sourceMappingURL=01-color-switcher.bd103e76.js.map
