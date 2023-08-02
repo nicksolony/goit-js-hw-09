aa94a51db8e19915beb2b21d73fe345a0858d8a0
nicksolony <nicksolony@gmail.com>
2023-08-01 22:03:48 -0400

    added color changing on a click

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,24 @@
 
 let startBtn = document.querySelector('[data-start]');
 let stopBtn = document.querySelector('[data-stop]');
+let body = document.querySelector('body');
+
+let bgColor = () =>{
+    body.style.backgroundColor=getRandomHexColor();
+    console.log('color change');
+};
 
 
-
-startBtn.addEventListener('click',()=>{
-    console.log('start');
-});
+startBtn.addEventListener('click',bgColor);
 
 stopBtn.addEventListener('click', () =>{
     console.log('stop');
 });
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+
+
