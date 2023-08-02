6c9386a4a18941a713bda18141521dd459225779
nicksolony <nicksolony@gmail.com>
2023-08-01 21:55:55 -0400

    set up btns and added event listeners

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
 
+let startBtn = document.querySelector('[data-start]');
+let stopBtn = document.querySelector('[data-stop]');
+
+
+
+startBtn.addEventListener('click',()=>{
+    console.log('start');
+});
+
+stopBtn.addEventListener('click', () =>{
+    console.log('stop');
+});
