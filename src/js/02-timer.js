2e527207603c5cb59ceeacab9227c189190d9f7e
nicksolony <nicksolony@gmail.com>
2023-08-01 23:18:43 -0400

    added check which date is selected

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,14 +8,25 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       console.log(selectedDates[0]);
+      checkFutureDate(selectedDates[0]);
     },
   };
 
 let datePicker = document.querySelector('#datetime-picker');
 
-datePicker.addEventListener('input', (e) => {
-    console.log(e.target.value);
-})
+let startBtn = document.querySelector('[data-start]');
+
+// datePicker.addEventListener('input', (e) => {
+//     console.log(e.target.value);
+// })
 
 flatpickr(datePicker, options);
 
+function checkFutureDate (selectDate) {
+    if (selectDate < new Date()) {
+        startBtn.disabled=true;
+        alert('Please choose a date in the future');
+    } else {
+        startBtn.disabled = false;
+    }
+};
